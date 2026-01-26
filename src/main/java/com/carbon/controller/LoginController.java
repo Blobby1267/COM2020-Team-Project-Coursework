8e6fbb0355f867401085f40ed1165087baf64754
Blobby1267 <benjaminhoskins101@gmail.com>
2026-01-26 15:18:02 +0000

    Fixing Java files, adding target folder (auto created from mavern, not sure why)

--- src/main/java/com/carbon/controller/LoginController.java
+++ src/main/java/com/carbon/controller/LoginController.java
@@ -1,4 +1,12 @@
 package com.carbon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.carbon.model.User;
+import com.carbon.repository.UserRepository;
+
 @Controller
 public class LoginController {
     @Autowired
