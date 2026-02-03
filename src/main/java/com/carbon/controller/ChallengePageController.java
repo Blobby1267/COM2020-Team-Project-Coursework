45f19e9d58c6da438721e19d4be18b0e45fcc504
MarkoParkinson <markostephenparkinson@gmail.com>
2026-02-03 11:51:53 +0000

    Challenges start

---
+++ src/main/java/com/carbon/controller/ChallengePageController.java
@@ -0,0 +1,18 @@
+package com.carbon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.carbon.model.Challenge;
+import com.carbon.repository.ChallengeRepository;
+
+import java.util.logging.Logger;
+
+public class ChallengePageController {
+    
+    private static final Logger LOGGER = Logger.getLogger(LoginController.class.getName());
+
+    @Autowired
+    private ChallengeRepository challengRepository;
+}
---
+++ src/main/java/com/carbon/model/Challenge.java
@@ -0,0 +1,73 @@
+package com.carbon.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity // Tells Java this maps to a database table
+@Table(name = "challenges")
+public class Challenge {
+    @Id  // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-incrementing ID
+    private Long id; // Unique identifier
+    private String title;
+    private String description;
+    private int points;
+    private String frequency;
+    private Date startDate;
+    private Date endDate;
+    private String scope;
+
+    //getters
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getPoints() {
+        return points;
+    }
+    public String getFrequency() {
+        return frequency;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+    public String getScope() {
+        return scope;
+    }
+
+    //setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+}
