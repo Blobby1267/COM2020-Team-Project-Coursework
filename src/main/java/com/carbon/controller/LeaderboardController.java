3be66e50638e0d92f5209498db93db8d6cabd547
Blobby1267 <benjaminhoskins101@gmail.com>
2026-02-09 18:15:42 +0000

    Finishing controller and changing html, waiting to test

--- src/main/java/com/carbon/controller/LeaderboardController.java
+++ src/main/java/com/carbon/controller/LeaderboardController.java
@@ -4,13 +4,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.carbon.repository.UserRepository;
+import java.util.List;
+import com.carbon.model.User;
 
+@Controller
 public class LeaderboardController {
+    @Autowired
+    private UserRepository userRepository;
+    
     private static final Logger LOGGER = Logger.getLogger(LoginController.class.getName());
 
     @GetMapping("/leaderboard")
     public String showLeaderboard(Model model) {
-        // Placeholder for leaderboard data retrieval logic
+        List<User> users = userRepository.findAll();
+        users.sort((u1, u2) -> Integer.compare(u2.getPoints(), u1.getPoints()));
+        model.addAttribute("users", users);
         LOGGER.info("Displaying the leaderboard.");
         return "leaderboard";
     }
